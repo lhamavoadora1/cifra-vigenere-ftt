d970a3853027c27042d6cfcff8f3e9ae2b628cca
lhamavoadora1 <53661607+lhamavoadora1@users.noreply.github.com>
2021-08-26 20:29:55 -0300

    add validations

--- Cifra de Vigenère.py
+++ Cifra de Vigenère.py
@@ -11,16 +11,36 @@ def build_tabula():
             line.append(alphabet[index])
         tabula_recta.append(line)
 
+def check_input(text):
+    for letter in text:
+        if letter not in alphabet:
+            return False
+    return True
+
 def input_phrase():
-    phrase = input('Digite uma simples frase: ').upper()
+    phrase = input('Digite uma frase simples: ').upper()
+    while not phrase or not check_input(phrase):
+        message = ''
+        if not phrase:
+            message = 'DIGITE uma frase simples: '
+        elif not check_input(phrase):
+            message = 'Existem caracteres desconhecidos na frase, digite uma frase simples: '
+        phrase = input(message).upper()
     phrase_len = len(phrase)
     return phrase, phrase_len
 
 def input_key(phrase_len):
     key = input('Digite a chave: ').upper()
     key_len = len(key)
-    while phrase_len < key_len:
-        key = input('A chave é maior do que a palavra!\nPor favor, digite uma chave menor: ').upper()
+    while phrase_len < key_len or not key or not check_input(key):
+        message = ''
+        if phrase_len < key_len:
+            message = 'A chave é maior do que a palavra!\nPor favor, digite uma chave menor: '
+        elif not key:
+            message = 'DIGITE a chave: '
+        elif not check_input(key):
+            message = 'Existem caracteres desconhecidos na chave, digite uma chave simples: '
+        key = input(message).upper()
         key_len = len(key)
     key_repetition = math.ceil(phrase_len / key_len)
     for i in range(1, key_repetition):
